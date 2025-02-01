e95d6226481b4e22e9f0872757a5a62f3120e596
Jonelle <jonelcubio021@gmail.com>
2025-02-01 13:47:09 +0800

    Add the form function to add ingredients

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 
 export function Main() {
 
-  const [ingredients, setIngredients] = useState([])
+  const [ingredients, setIngredients] = useState([]);
 
   const ingredientsListItems = ingredients.map(ingredient => (
       <li key={ingredient}>{ingredient}</li>
   ));
 
-function handleSubmit(event) {
-  event.preventDefault()
+
+  function handleSubmit(event) {
+  event.preventDefault();
   const formData = new FormData(event.currentTarget)
 
-  const newIngredient = formData.get("ingredient")
+  const newIngredient = formData.get("ingredient");
   
-  setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+  setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
 }
 
 
@@ -23,14 +24,16 @@ function handleSubmit(event) {
     <>
     <div className="main-con">
 
-    <form className="add-ingredient-form">
+    <form className="add-ingredient-form" onClick={handleSubmit}>
 
       <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" className="input-ing"
       name="ingredient"/>
 
       <button className="ingr-btn">+ Add ingredient</button>
       </form>
-
+      <ul>
+        {ingredientsListItems}
+      </ul>
     </div>
     </>
   );
