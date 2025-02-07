426d556ca390bb07fb9ac7c18f1aac61c4739d7e
Jonelle <jonelcubio021@gmail.com>
2025-02-07 20:02:31 +0800

    Another batch of edit in the inputs

--- api/huggingface-api.js
+++ api/huggingface-api.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
           { role: "user", content: `I have ${ingredients.join(', ')}. Please give me a recipe you'd recommend I make!` }  
         ] */
 
-        inputs: `I have ${ingredients.join(', ')}. Please give me a recipe you'd recommend I make!`,
+        inputs: `You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. I have ${ingredients.join(', ')}. Please give me a recipe you'd recommend I make!`,
       }),
     });
 
