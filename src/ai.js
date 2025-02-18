456c45c0559f90df7176377cd009683a57bd8f1e
Jonelle <jonelcubio021@gmail.com>
2025-02-18 17:26:10 +0800

    Edited some changed in ai.js

--- src/ai.js
+++ src/ai.js
@@ -1,3 +1,8 @@
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+`
+
+
 export async function getRecipeFromMistral(ingredientsArr) {
   const ingredientsString = ingredientsArr.join(", ");
 
@@ -5,7 +10,12 @@ export async function getRecipeFromMistral(ingredientsArr) {
     const response = await fetch(`/api/huggingface-api`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ ingredients: ingredientsArr })
+      //body: JSON.stringify({ ingredients: ingredientsArr });
+
+      messages: [
+        { role: "system", content: SYSTEM_PROMPT },
+        { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+      ]
     });
 
     // Check if the response is OK (status 200)
