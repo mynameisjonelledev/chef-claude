a11aa4b076cab0eb339e8c3b2073429bba402a98
Jonelle <jonelcubio021@gmail.com>
2025-02-19 12:00:08 +0800

    Displays Chef Jonelle is thinking ... while waiting for recipe

--- src/components/JonelleRecipe.jsx
+++ src/components/JonelleRecipe.jsx
@@ -3,13 +3,7 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown'
 
 export function JonelleRecipe(props) {
-  if (!props.recipe) {
-    return (
-      <section className="suggested-recipe-container" aria-live="polite">
-        <h2>Chef Jonelle is thinking...</h2>
-      </section>
-    );
-  }
+  
 
   let recipeData = props.recipe;
   
@@ -53,8 +47,12 @@ export function JonelleRecipe(props) {
 
   return (
     <section className="suggested-recipe-container" aria-live="polite">
-      <h2>Chef Jonelle Recommends:</h2>
-      <ReactMarkdown>{recipeContent}</ReactMarkdown>
+      {props.recipe ? (
+        <>
+        <h2>Chef Jonelle Recommends:</h2>
+        <ReactMarkdown>{props.recipe}</ReactMarkdown>
+        </>
+      ) : (<h2>Chef Jonelle is thinking ...</h2>)}
     </section>
   );
 }
